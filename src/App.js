7f09ca5947d2b179ed464c099e6004e7e89591f5
Ayten Abdullayeva <ayten.a@bestcomp.net>
2021-10-17 15:26:06 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
 
 function App() {
+ const [count,setCount]=useState(0);
+ useEffect(() => {
+  console.log("effect is called")
+   return () => {
+    console.log("effect is clean up")
+   }
+ }, [count])
+ console.log(count);
+ const incrementCount=()=>{
+    setCount(prevCount=>prevCount+1);
+    console.log(count);
+    setCount(prevCount=>prevCount+1);
+    console.log(count);
+ }
+ const decrementCount=()=>{
+  setCount(count-1)
+ }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Interview preparation</h1>
+       
+       <button onClick={decrementCount} className="btn btn-secondary">-</button>
+       <span style={{padding:'10px'}}>Count is {count}</span>
+       <button  onClick={incrementCount} className="btn btn-secondary">+</button>
+       
     </div>
   );
 }
